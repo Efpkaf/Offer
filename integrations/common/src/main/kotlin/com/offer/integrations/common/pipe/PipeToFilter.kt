50945d0cd52e16c85a1b657cebea72a022492fac
piotr.a@practi.net <piotr.a@practi.net>
2021-06-22 21:49:39 +0200

    updated gradle files v2

--- integrations/common/src/main/kotlin/com/offer/integrations/common/pipe/PipeToFilter.kt
+++ integrations/common/src/main/kotlin/com/offer/integrations/common/pipe/PipeToFilter.kt
@@ -1,7 +1,6 @@
 package com.offer.integrations.common.pipe
 
 import com.offer.searchingPhrase.PhrasesDto
-import com.offer.step.step.Pipe
 
 fun PhrasesDto.toSearchingItemPropertiesPipe(): List<SearchingItemProperties> {
     return PhraseToSearchingItemPropertiesPipe(this).process()
